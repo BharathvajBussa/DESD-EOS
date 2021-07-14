e01062ed10325e46a0b6c9f1ac32c126f13e1966
Bussa Bharathvaj <bussabharathvaj@gmail.com>
2021-07-14 13:14:33 +0530

    file handling demo

---
+++ file/rawfile.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+int fd;
+fd = open("desd.txt", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);\
+if (-1 == fd)
+{    perror("Error opening file");
+     exit(EXIT_FAILURE);
+}
+write(fd, "DESD W\n", 7);
+close(fd);
+exit(EXIT_SUCCESS);
+}
