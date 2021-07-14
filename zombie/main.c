3fe5eee0e7140bc1b82a868837e6530940c4490a
Bussa Bharathvaj <bussabharathvaj@gmail.com>
2021-07-14 18:49:11 +0530

    Zombie demo

---
+++ zombie/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t id;
+    id=fork();
+    if(0==id)
+    {
+        printf("Child Process\n");
+        printf("Child Process ID: %d\n",getpid());
+        printf("Child Process Parent ID: %d\n",getppid());
+    }
+    else
+    {
+        printf("Parent Process\n");
+        sleep(8);
+        printf("Parent Process ID: %d\n",getpid());
+        printf("Parent Process Parent ID: %d\n",getppid());  
+    }
+}
